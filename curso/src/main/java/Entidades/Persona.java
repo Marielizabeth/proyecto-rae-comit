35bf0969b96988b57d0b93862cd5e11a4f70b178
Marielizabeth <marielaerobles@gmail.com>
2021-03-31 17:15:04 -0300

    Avances en clase Persona

--- curso/src/main/java/Entidades/Persona.java
+++ curso/src/main/java/Entidades/Persona.java
@@ -3,25 +3,29 @@ package Entidades;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
-
-
-
+@NoArgsConstructor
+@Builder(setterPrefix = "set")
+@ToString
 public class Persona implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-private string apellido;
-private string nombre;
-private string mail;
-private List <Telefono> telefonos;
+private String apellido;
+private String nombre;
+private String mail;
+private String ciudad;
+private List<Telefono> telefonos;
 
-public Persona() {
-	// TODO Auto-generated constructor stub
-}
+
+
 
 
 
--- curso/src/main/java/com/comit/curso/MainApplication.java
+++ curso/src/main/java/com/comit/curso/MainApplication.java
@@ -1,14 +1,21 @@
 package com.comit.curso;
 
+import java.util.ArrayList;
+
 import Entidades.Persona;
 
 public class MainApplication {
 
 	public static void main(String[] args) {
-		Persona per = new Persona();
+		Persona per = Persona.builder()
+					.setApellido("Robles")
+					.setNombre("Mariela Elizabeth")
+					.setMail("m.elizabeth.robles87@gmail.com")
+					.setCiudad("Mar del Plata")
+					.setTelefono(new ArrayList<Telefono>()).build();
 		per.setApellido("Robles");
 		
-		System.out.println(per.getApellido());
+		System.out.println(per.toString());
 
 	}
 
