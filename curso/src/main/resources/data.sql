5053c75a1bcfbd604cb22d68eefc9fb656eb8c6b
Marielizabeth <marielaerobles@gmail.com>
2021-04-04 17:21:09 -0300

    Elimine archivo data

--- curso/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Persona(apellido, nombre, mail) VALUES("Robles", "Mariela", "marielaerobles@gmail.com");
-INSERT INTO Telefono(codigo_area, numero, is_celular) VALUES("11", "60389624", false);
-INSERT INTO Telefono(codigo_area, numero, is_celular) VALUES("223", "4565606", true);
-
-INSERT INTO persona_telefonos(persona_id, telefonos_id) VALUES(1,1), (1,2);
